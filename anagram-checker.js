f0dc56864370f0ac98a916ec8b03cd271cc46f62
mossa <martin.ossa.8@gmail.com>
2023-01-25 14:06:31 +0100

    solve challenge

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,3 +1,19 @@
-module.exports = function (str) {
-  // write your code here
+module.exports = 
+function anagramCheck(str1, str2) {
+
+  let orderStr1 = str1.toLowerCase().replace(/\W/g, "").split('').sort().join('');
+  let orderStr2 = str2.toLowerCase().replace(/\W/g, "").split('').sort().join('');
+
+  if (orderStr1 === orderStr2) {
+    return 'true';
+  } else {
+    return 'false';
+  }
 }
+
+// create two variables to compare - done
+// substract variables into single characters
+// eliminate all non alphanumeric
+// turn all to lower case
+// order them alphabetically
+// if str1 = str2, true else false

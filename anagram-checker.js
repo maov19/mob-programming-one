73956514f4bad4666daaf3aa7a884a2fc14e0c18
mossa <martin.ossa.8@gmail.com>
2023-03-21 08:32:28 +0100

    First attempt

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,3 +1,23 @@
-module.exports = function (str) {
+// You'll need to remove **all non-alphanumeric characters** (punctuation, spaces and symbols) - DONE
+// and turn everything into the same case (lower or upper case) in order to check for **anagrams**. - DONE
+//order both words alphabetically- DONE
+//check if they are ===
+module.exports = 
+function anagramFinder(strOne, strTwo) {
   // write your code here
+  strOne = strOne.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
+  strTwo = strTwo.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
+
+  if (strOne === strTwo) {
+    return true;    
+  } else {
+    return false;
+  }
 }
+
+let result = anagramFinder("friend", "finder");
+console.log(result); //true
+
+definr === definr
+
+
